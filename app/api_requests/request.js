284c23ef1b4e573f8ae56c290956dd61f08ea564
Arijeet10 <arijeetsocial@gmail.com>
2024-03-28 16:10:39 +0530

    Task Board App CRUD

--- app/api_requests/request.js
+++ app/api_requests/request.js
@@ -1,4 +1,4 @@
-const url=process.env.NEXT_PUBLIC_ROOT_URL;
+const url=process.env.NEXT_PUBLIC_ROOT_URL || "http://localhost:3000/api/";
 
 
 export const getApiResponse=async()=>{//api request to GET task data
